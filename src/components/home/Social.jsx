78be53dcdd0026cc93923dd139b8ab75a427d741
shunmuga-raja <84874487+shunmuga-raja@users.noreply.github.com>
2023-05-28 19:44:33 +0530

    home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+        {/*insta icons*/}
+        <a href='https://www.instagram.com/' className='home__social-icon' target='_blank'>
+        <i class="uil uil-instagram"></i>
+        </a>
+
+        {/*dribble icons*/}
+        <a href='https://dribbble.com/' className='home__social-icon' target='_blank'>
+        <i class="uil uil-dribbble"></i>  
+        </a>
+
+        {/*github icons*/}
+        <a href='https://github.com/' className='home__social-icon' target='_blank'>
+        <i class="uil uil-github-alt"></i>   
+        </a>
+    </div>
+  )
+}
+
+export default Social

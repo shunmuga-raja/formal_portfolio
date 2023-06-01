9fb93682f7e4d6b34b221938f217fe03f4329387
shunmuga-raja <84874487+shunmuga-raja@users.noreply.github.com>
2023-06-01 23:07:21 +0530

    commi

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Social = () => {
   return (
     <div className='home__social'>
-        {/*insta icons*/}
+        {/*insta icon*/}
         <a href='https://www.instagram.com/' className='home__social-icon' target='_blank'>
         <i class="uil uil-instagram"></i>
         </a>

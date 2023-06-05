89ec251ecb7deaf5b9422aef7adf3a1b7171bc76
shunmuga-raja <84874487+shunmuga-raja@users.noreply.github.com>
2023-06-05 19:37:53 +0530

    about me

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header2 from './components/header/Header2';
 import Home from './components/home/Home';
+import About from './components/about/About';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
 
     <main className='main'>
       <Home />
+      <About />
     </main>
     </>
   );
---
+++ src/components/about/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./about.css";
+import AboutImg from "../../assets/about.JPG";
+import CV from "../../assets/John-Cv.pdf";
+
+
+const About = () => {
+  return (
+    <section className="about section" id="about">
+        <h2 className="section__title">About Me</h2>
+        <span className="section__subtitle">My introduction</span>
+
+        <div className="about__container container grid">
+            <img src={AboutImg} alt="" className="about__img" />
+
+            <div className="about__data"></div>
+        </div>
+    </section>
+  )
+}
+
+export default About

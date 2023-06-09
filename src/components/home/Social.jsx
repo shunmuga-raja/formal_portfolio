9440b6bdadd3b8238f67119e6f665b6f10ded672
shunmuga-raja <84874487+shunmuga-raja@users.noreply.github.com>
2023-06-09 13:49:51 +0530

    change link

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,13 +4,13 @@ const Social = () => {
   return (
     <div className='home__social'>
         {/*insta icon*/}
-        <a href='https://www.instagram.com/' className='home__social-icon' target='_blank'>
-        <i class="uil uil-instagram"></i>
+        <a href='https://portfolio-92877.web.app' className='home__social-icon' target='_blank'>
+        <i class="uil uil-dribbble"></i>
         </a>
 
         {/*dribble icons*/}
-        <a href='https://dribbble.com/' className='home__social-icon' target='_blank'>
-        <i class="uil uil-dribbble"></i>  
+        <a href='https://www.linkedin.com/in/shanmuga-raja-914124219' className='home__social-icon' target='_blank'>
+        <i class="uil uil-linkedin"></i> 
         </a>
 
         {/*github icons*/}
